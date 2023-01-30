afb03aa1c033e3c3106e51d5d27b3a2c85957673
pdostonh <pdostonh@gmail.com>
2023-01-30 23:55:33 +0500

    developed some functions

---
+++ script.js
@@ -0,0 +1,8 @@
+function getComputerChoice() {
+    let options = ["rock", "paper", "scissors"];
+    return options[Math.floor(Math.random()*options.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    //should return you win that beats this
+}
